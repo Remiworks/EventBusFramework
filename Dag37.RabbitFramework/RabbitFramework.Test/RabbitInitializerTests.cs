4c2c87e4b4dde300874adbac2f9261cdc8703701
Rene Buunk <renebuunk1996@gmail.com>
2017-11-13 15:34:07 +0100

    Basic tests

---
+++ Dag37.RabbitFramework/RabbitFramework.Test/RabbitInitializerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitFramework.Test
+{
+    [TestClass]
+    public class RabbitInitializerTests
+    {
+        private Mock<IBusProvider> _busProviderMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _busProviderMock = new Mock<IBusProvider>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void InitializeCallsCreateConnection()
+        {
+            _busProviderMock.Setup(b => b.CreateConnection());
+            RabbitInitializer target = new RabbitInitializer(_busProviderMock.Object);
+
+            target.Initialize();
+
+            _busProviderMock.Verify(b => b.CreateConnection(), Times.AtMostOnce);
+        }
+
+        [TestMethod]
+        public void RegisterQueueToCallback()
+        {
+            _busProviderMock.Setup(m => m.BasicConsume("queue", It.IsAny<EventReceivedCallback>()));
+
+            RabbitInitializer target = new RabbitInitializer(_busProviderMock.Object);
+
+            target.RegisterQueueToCallback("queue");
+
+            _busProviderMock.Verify(m => m.BasicConsume("queue", It.IsAny<EventReceivedCallback>()), Times.AtMostOnce);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
---
+++ Dag37.RabbitFramework/RabbitFramework/RabbitInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RabbitFramework
+{
+    public class RabbitInitializer
+    {
+        private IBusProvider _busProvider;
+
+        public RabbitInitializer(IBusProvider busProvider)
+        {
+            _busProvider = busProvider;
+        }
+
+        public void Initialize()
+        {
+            _busProvider.CreateConnection();
+        }
+
+        public void RegisterQueueToCallback(string queueName)
+        {
+            _busProvider.BasicConsume(queueName, RunCallback);
+        }
+
+        public void RunCallback(EventMessage message)
+        {
+
+        }
+    }
+}
