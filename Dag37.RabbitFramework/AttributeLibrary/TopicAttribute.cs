cf34172f9aa346d7e559825e6d46b9d6f1c50d97
Rene Buunk <renebuunk1996@gmail.com>
2017-11-13 14:45:02 +0100

    Attribute Eventlistener and Topic

---
+++ Dag37.RabbitFramework/AttributeLibrary/TopicAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AttributeLibrary
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TopicAttribute : Attribute
+    {
+        public string Topic { get; }
+
+        public TopicAttribute(string topic)
+        {
+            Topic = topic;
+        }
+    }
+}
