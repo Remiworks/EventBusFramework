083356f8b263ac283b18cae320897c377dbc8734
Rene Buunk <renebuunk1996@gmail.com>
2017-11-21 13:25:29 +0100

    testprojects

---
+++ Dag37.RabbitFramework/RabbitmqPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RabbitmqPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private void OpenRabbitConnection()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = Host,
+                Port = Port,
+                UserName = UserName,
+                Password = Password
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(ExchangeName, TopicType);
+        }
+
+        private void SendRabbitEvent(string topic, string json)
+        {
+            _channel.BasicPublish(exchange: ExchangeName,
+                                 routingKey: topic,
+                                 basicProperties: null,
+                                 body: Encoding.UTF8.GetBytes(json));
+        }
+    }
+}
---
+++ Dag37.RabbitFramework/RabbitmqReceiver/Mather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitmqReceiver
+{
+    public class Mather
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Calculate called!");
+        }
+
+    }
+}
