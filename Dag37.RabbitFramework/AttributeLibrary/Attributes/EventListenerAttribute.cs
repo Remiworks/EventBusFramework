5265634410722aef2a42eb98d1846f0168b785d4
Mike Derksen <mikederksen5@live.nl>
2017-11-21 20:20:37 +0100

    Moved attributes to own namespace

---
+++ Dag37.RabbitFramework/AttributeLibrary/Attributes/EventListenerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AttributeLibrary.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class EventListenerAttribute : Attribute
+    {
+        public string QueueName { get; }
+
+        public EventListenerAttribute(string queueName)
+        {
+            QueueName = queueName;
+        }
+    }
+}
---
+++ Dag37.RabbitFramework/AttributeLibrary/Attributes/TopicAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AttributeLibrary.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TopicAttribute : Attribute
+    {
+        public string Topic { get; }
+
+        public TopicAttribute(string topic)
+        {
+            Topic = topic;
+        }
+    }
+}
