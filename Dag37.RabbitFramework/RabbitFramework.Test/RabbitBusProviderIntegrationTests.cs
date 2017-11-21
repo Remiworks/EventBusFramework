b53dfbe752f3a125a1c8c2eb9ef63e5cec409cbd
Mike Derksen <MikeD@minor.infosupport.edu>
2017-11-21 16:04:02 +0100

    Fixed integration tests

--- Dag37.RabbitFramework/RabbitFramework.Test/RabbitBusProviderIntegrationTests.cs
+++ Dag37.RabbitFramework/RabbitFramework.Test/RabbitBusProviderIntegrationTests.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -90,7 +92,7 @@ namespace RabbitFramework.Test
                     Type = TopicType
                 };
 
-                ConsumeRabbitEvent(queue, topic, (sender, args) =>
+                ConsumeRabbitEvent(queue, (sender, args) =>
                 {
                     waitHandle.Set();
                     passedArgs = args;
@@ -113,7 +115,7 @@ namespace RabbitFramework.Test
             {
                 string queue = UniqueQueue();
                 string topic = UniqueTopic();
-                
+
                 EventMessage receivedMessage = null;
                 ManualResetEvent waitHandle = new ManualResetEvent(false);
                 EventReceivedCallback eventReceivedCallback = (message) =>
@@ -135,6 +137,83 @@ namespace RabbitFramework.Test
 
                 waitHandle.WaitOne(2000).ShouldBeTrue();
                 receivedMessage.JsonMessage.ShouldBe(sentMessage.JsonMessage);
+            }
+        }
+
+        [TestMethod]
+        public void CommandCanBeReceived()
+        {
+            using (var sut = new RabbitBusProvider(BusOptions))
+            {
+                string queue = UniqueQueue();
+                string correlationId = new Guid().ToString();
+
+                CommandStub sentCommand = new CommandStub { Value = "SomeValue" };
+                string sentCommandJson = JsonConvert.SerializeObject(sentCommand);
+
+                CommandStub receivedCommand = null;
+                ManualResetEvent waitHandle = new ManualResetEvent(false);
+                CommandReceivedCallback<CommandStub> commandReceivedCallback = (command) =>
+                {
+                    receivedCommand = command;
+                    waitHandle.Set();
+
+                    return "Something";
+                };
+
+                sut.SetupRpcListener(queue, commandReceivedCallback);
+                SendRabbitEventToQueue(queue, correlationId, sentCommandJson);
+
+                waitHandle.WaitOne(2000).ShouldBeTrue();
+                receivedCommand.Value.ShouldBe(sentCommand.Value);
+            }
+        }
+
+        [TestMethod]
+        public void CommandCanBeSent()
+        {
+            using (var sut = new RabbitBusProvider(BusOptions))
+            {
+                string queue = UniqueQueue();
+                CommandStub sentCommand = new CommandStub { Value = "SomeValue" };
+
+                BasicDeliverEventArgs receivedEventArgs = null;
+                ManualResetEvent waitHandle = new ManualResetEvent(false);
+                EventHandler<BasicDeliverEventArgs> commandCallback = (sender, eventArgs) =>
+                {
+                    receivedEventArgs = eventArgs;
+                    waitHandle.Set();
+                };
+
+                ConsumeRabbitEvent(queue, commandCallback);
+
+                // Timeout exception is expected. We dont send a response to the calling party
+                var exception = Should.Throw<AggregateException>(() => sut.Call<string>(queue, sentCommand, 0).Wait());
+                exception.InnerException.ShouldBeOfType<TimeoutException>();
+
+                waitHandle.WaitOne(2000).ShouldBeTrue();
+
+                receivedEventArgs.BasicProperties.CorrelationId.ShouldNotBeNullOrEmpty();
+                receivedEventArgs.BasicProperties.ReplyTo.ShouldNotBeNullOrEmpty();
+
+                string commandJson = Encoding.UTF8.GetString(receivedEventArgs.Body);
+                CommandStub receivedCommand = JsonConvert.DeserializeObject<CommandStub>(commandJson);
+                receivedCommand.Value.ShouldBe(sentCommand.Value);
+            }
+        }
+
+        [TestMethod]
+        public void CommandCanBeSentAndReceived()
+        {
+            using (var sut = new RabbitBusProvider(BusOptions))
+            {
+                string queue = UniqueQueue();
+                CommandStub sentCommand = new CommandStub { Value = "SomeValue" };
+                
+                sut.SetupRpcListener<CommandStub>(queue, (command) => command.Value.Reverse().ToString());
+                string result = sut.Call<string>(queue, sentCommand).Result;
+
+                result.ShouldBe(sentCommand.Value.Reverse().ToString());
             }
         }
 
@@ -164,6 +243,18 @@ namespace RabbitFramework.Test
             _channel.ExchangeDeclare(ExchangeName, TopicType);
         }
 
+        private void SendRabbitEventToQueue(string queue, string correlationId, string json)
+        {
+            var properties = _channel.CreateBasicProperties();
+            properties.CorrelationId = correlationId;
+
+            _channel.BasicPublish(
+                exchange: "",
+                routingKey: queue,
+                basicProperties: properties,
+                body: Encoding.UTF8.GetBytes(json));
+        }
+
         private void SendRabbitEventToExchange(string topic, string json)
         {
             _channel.BasicPublish(exchange: ExchangeName,
@@ -172,10 +263,9 @@ namespace RabbitFramework.Test
                                  body: Encoding.UTF8.GetBytes(json));
         }
 
-        private void ConsumeRabbitEvent(string queue, string topic, EventHandler<BasicDeliverEventArgs> callback)
+        private void ConsumeRabbitEvent(string queue, EventHandler<BasicDeliverEventArgs> callback)
         {
             _channel.QueueDeclare(queue: queue, exclusive: false);
-            _channel.QueueBind(queue, ExchangeName, topic);
 
             var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += callback;
