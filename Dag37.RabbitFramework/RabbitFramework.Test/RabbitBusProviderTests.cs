4004f2359e389e8409497d9ba18fb1fba5b13380
Mike Derksen <mikederksen5@live.nl>
2017-11-21 09:49:42 +0000

    Updated RabbitBusProviderTests.cs with old content

--- Dag37.RabbitFramework/RabbitFramework.Test/RabbitBusProviderTests.cs
+++ Dag37.RabbitFramework/RabbitFramework.Test/RabbitBusProviderTests.cs
@@ -12,13 +12,50 @@ namespace RabbitFramework.Test
 
         private RabbitBusProvider _sut;
 
-       
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sut = new RabbitBusProvider(_busOptionsMock.Object);
+        }
+
         [TestMethod]
         public void ConstructorSetsBusOptions()
         {
             _sut.BusOptions.ShouldBe(_busOptionsMock.Object);
         }
 
-       
+        [TestMethod]
+        public void BasicConsumeThrowsArgumentExceptionWhenQueueNameIsNull()
+        {
+            EventReceivedCallback callback = new EventReceivedCallback((message) => { });
+
+            var exception = Should.Throw<ArgumentException>(() => _sut.BasicConsume(null, callback));
+            exception.Message.ShouldBe("queueName");
+        }
+
+        [TestMethod]
+        public void BasicConsumeThrowsArgumentExceptionWhenQueueNameIsEmpty()
+        {
+            EventReceivedCallback callback = new EventReceivedCallback((message) => { });
+
+            var exception = Should.Throw<ArgumentException>(() => _sut.BasicConsume("", callback));
+            exception.Message.ShouldBe("queueName");
+        }
+
+        [TestMethod]
+        public void BasicConsumeThrowsArgumentExceptionWhenQueueNameIsWhiteSpace()
+        {
+            EventReceivedCallback callback = new EventReceivedCallback((message) => { });
+
+            var exception = Should.Throw<ArgumentException>(() => _sut.BasicConsume(" ", callback));
+            exception.Message.ShouldBe("queueName");
+        }
+
+        [TestMethod]
+        public void BasicConsumeThrowsArgumentExceptionWhenCallbackIsNull()
+        {
+            var exception = Should.Throw<ArgumentException>(() => _sut.BasicConsume("SomeQueue", null));
+            exception.Message.ShouldBe("callback");
+        }       
     }
 }
