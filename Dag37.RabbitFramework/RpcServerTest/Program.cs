dea0e2cec3f06ae43949ba921a327c780d06b37d
Mike Derksen <MikeD@minor.infosupport.edu>
2017-11-21 10:28:51 +0100

    Re-added fib code

--- Dag37.RabbitFramework/RpcServerTest/Program.cs
+++ Dag37.RabbitFramework/RpcServerTest/Program.cs
@@ -1,10 +1,14 @@
 ﻿using RabbitFramework;
+using RabbitMQ.Client;
 using System;
 
 namespace RpcServerTest
 {
     public static class Program
     {
+        private static IConnection _connection;
+        private static IModel _channel;
+
         public static void Main()
         {
             using (IBusProvider server = new RabbitBusProvider(new BusOptions()))
@@ -14,6 +18,21 @@ namespace RpcServerTest
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private static object Fib(SomeCommand n)
+        {
+            return CalculateFib(n.Value);
+        }
+
+        private static int CalculateFib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return CalculateFib(n - 1) + CalculateFib(n - 2);
         }
     }
 
