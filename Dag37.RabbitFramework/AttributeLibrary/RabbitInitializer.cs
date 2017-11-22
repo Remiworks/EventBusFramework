ddaad8286fdd6a267d9e0a5319346899c82dc4bd
Rene Buunk <renebuunk1996@gmail.com>
2017-11-22 21:44:00 +0100

    added logger to extension

--- Dag37.RabbitFramework/AttributeLibrary/RabbitInitializer.cs
+++ Dag37.RabbitFramework/AttributeLibrary/RabbitInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RabbitFramework;
 using System;
---
+++ Dag37.RabbitFramework/RabbitFramework/RabbitLogging.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitFramework
+{
+    public static class RabbitLogging
+    {
+        public static ILoggerFactory LoggerFactory { get; set; } = new LoggerFactory();
+        public static ILogger CreateLogger<T>() =>
+          LoggerFactory.CreateLogger<T>();
+    }
+}
