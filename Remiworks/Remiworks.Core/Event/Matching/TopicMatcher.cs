cb6351a279943a2416b195a48605b87627af4e9b
Mike Derksen <m.derksen@avisi.nl>
2017-12-11 13:20:14 +0100

    Added TopicMatcher with tests

---
+++ Remiworks/Remiworks.Core/Event/Matching/TopicMatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Remiworks.Core.Event.Matching
+{
+    public static class TopicMatcher
+    {
+        private const string RegexHashtag = @"\w+(\.\w+)*";
+        private const string RegexStar = @"[\w]+";
+        
+        public static IEnumerable<string> Match(string topic, params string[] topicsToMatch)
+        {
+            if(topicsToMatch == null || !topicsToMatch.Any() || string.IsNullOrWhiteSpace(topic))
+            {
+                return new List<string>();
+            }
+
+            return topicsToMatch
+                .Where(topicToMathch => IsMatch(topic, topicToMathch))
+                .ToList();
+        }
+
+        private static bool IsMatch(string topic, string topicToMatch)
+        {
+            var parsedTopic = topicToMatch
+                .Replace(".", "\\.")
+                .Replace("*", RegexStar)
+                .Replace("#", RegexHashtag);
+
+            return Regex.IsMatch(topic, $"^{parsedTopic}$");
+        }
+    }
+}
