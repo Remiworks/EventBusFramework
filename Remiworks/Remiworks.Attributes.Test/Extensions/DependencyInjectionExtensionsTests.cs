7e4ca4dc48c6079eb8b0e9888d87e2d464d54b32
Mike Derksen <m.derksen@avisi.nl>
2017-12-12 22:47:26 +0100

    Added parameter check in extension method and added corresponding test

---
+++ Remiworks/Remiworks.Attributes.Test/Extensions/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Remiworks.Attributes.Extensions;
+using Shouldly;
+
+namespace Remiworks.Attributes.Test.Extensions
+{
+    [TestClass]
+    public class DependencyInjectionExtensionsTests
+    {
+        private readonly Mock<IServiceProvider> _serviceProviderMock = new Mock<IServiceProvider>();
+        
+        [TestMethod]
+        public void UseAttributesThrows_ArgumentNullException_WhenServiceProviderIsNull()
+        {
+            Should.Throw<ArgumentNullException>(() => 
+                _serviceProviderMock.Object.UseAttributes());
+        }
+    }
+}
