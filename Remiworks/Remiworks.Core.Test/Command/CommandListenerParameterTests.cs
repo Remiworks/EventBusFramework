e28829f9983e28de822bc6a6a7dbd2c904620d74
Mike Derksen <m.derksen@avisi.nl>
2017-12-16 22:52:03 +0100

    Added command listener with parameter validation

---
+++ Remiworks/Remiworks.Core.Test/Command/CommandListenerParameterTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Remiworks.Core.Command;
+using Remiworks.Core.Test.Stubs;
+using Shouldly;
+
+namespace Remiworks.Core.Test.Command
+{
+    [TestClass]
+    public class CommandListenerParameterTests
+    {
+        private const string QueueNameParameter = "queueName";
+        private const string KeyParameter = "key";
+        private const string CallbackParameter = "callback";
+        private const string ParameterTypeParameter = "parameterType";
+        
+        private const string QueueName = "someQueue";
+        private const string Key = "some.key";
+        
+        private CommandListener _sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sut = new CommandListener(new Mock<IBusProvider>().Object);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentNullException_WhenQueueNameIsNull()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() =>
+                _sut.SetupCommandListenerAsync(null, Key, new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentException_WhenQueueNameIsEmpty()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync("", Key, new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentException_WhenQueueNameIsWhiteSpace()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(" ", Key, new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentNullException_WhenKeyIsNull()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, null, new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentException_WhenKeyIsEmpty()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, "", new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentException_WhenKeyIsWhiteSpace()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, " ", new Mock<CommandReceivedCallback<Person>>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncGenericThrows_ArgumentNullException_WhenCallbackIsNull()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync<Person>(QueueName, Key, null)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(CallbackParameter);
+        }
+       
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentNullException_WhenQueueNameIsNull()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() =>
+                _sut.SetupCommandListenerAsync(null, Key, new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentException_WhenQueueNameIsEmpty()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync("", Key, new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentException_WhenQueueNameIsWhiteSpace()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(" ", Key, new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(QueueNameParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentNullException_WhenKeyIsNull()
+        {
+            var exception = Should.Throw<ArgumentNullException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, null, new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentException_WhenKeyIsEmpty()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, "", new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentException_WhenKeyIsWhiteSpace()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, " ", new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(KeyParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentNullException_WhenCallbackIsNull()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, Key, null, new Mock<Type>().Object)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(CallbackParameter);
+        }
+
+        [TestMethod]
+        public void SetupCommandListenerAsyncThrows_ArgumentNullException_WhenTypeIsNull()
+        {
+            var exception = Should.Throw<ArgumentException>(() =>
+                _sut.SetupCommandListenerAsync(QueueName, Key, new Mock<CommandReceivedCallback>().Object, null)
+                    .Wait());
+            
+            exception.ParamName.ShouldBe(ParameterTypeParameter);
+        }
+    }
+}
---
+++ Remiworks/Remiworks.Core/Command/CommandListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using EnsureThat;
+
+namespace Remiworks.Core.Command
+{
+    public class CommandListener : ICommandListener
+    {
+        private readonly IBusProvider _busProvider;
+        
+        public CommandListener(IBusProvider busProvider)
+        {
+            _busProvider = busProvider;
+        }
+        
+        public Task SetupCommandListenerAsync<TParam>(string queueName, string key, CommandReceivedCallback<TParam> callback)
+        {
+            EnsureArg.IsNotNullOrWhiteSpace(queueName, nameof(queueName));           
+            EnsureArg.IsNotNullOrWhiteSpace(key, nameof(key));
+            EnsureArg.IsNotNull(callback, nameof(callback));
+
+            return SetupCommandListenerAsync(
+                queueName, 
+                key,
+                parameter => callback((TParam) parameter),
+                typeof(TParam));
+        }
+
+        public Task SetupCommandListenerAsync(string queueName, string key, CommandReceivedCallback callback, Type parameterType)
+        {
+            EnsureArg.IsNotNullOrWhiteSpace(queueName, nameof(queueName));           
+            EnsureArg.IsNotNullOrWhiteSpace(key, nameof(key));
+            EnsureArg.IsNotNull(callback, nameof(callback));
+            EnsureArg.IsNotNull(parameterType, nameof(parameterType));
+
+            return null;
+        }
+    }
+}
