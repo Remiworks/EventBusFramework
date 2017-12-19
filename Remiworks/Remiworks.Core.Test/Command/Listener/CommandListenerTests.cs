3520259b9824469a1684e95c443d3e06eae849b3
Mike Derksen <m.derksen@avisi.nl>
2017-12-19 15:26:51 +0100

    Added more unit tests for CommandListener

--- Remiworks/Remiworks.Core.Test/Command/Listener/CommandListenerTests.cs
+++ Remiworks/Remiworks.Core.Test/Command/Listener/CommandListenerTests.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using Remiworks.Core.Command.Listener;
 using Remiworks.Core.Command.Listener.Callbacks;
 using Remiworks.Core.Models;
+using Remiworks.Core.Test.Stubs;
+using Shouldly;
 
 namespace Remiworks.Core.Test.Command.Listener
 {
@@ -12,7 +17,21 @@ namespace Remiworks.Core.Test.Command.Listener
     public class CommandListenerTests
     {
         private const string QueueName = "someQueue";
+        private const string ReplyQueueName = "someReplyQueue";
         private const string Key = "some.key";
+        private const string ReplyKey = "some.key.Reply";
+        private const ulong DeliveryTag = 12345;
+
+        private static readonly Person Person = new Person { Name = "Jan Jannes" };
+
+        private static readonly EventMessage IncomingEventMessage = new EventMessage
+        {
+            CorrelationId = new Guid("F9168C5E-CEB2-4faa-B6BF-329BF39FA1E4"),
+            JsonMessage = JsonConvert.SerializeObject(Person),
+            ReplyQueueName = ReplyQueueName,
+            RoutingKey = Key,
+            DeliveryTag = DeliveryTag
+        };
         
         private readonly Mock<IBusProvider> _busProviderMock = new Mock<IBusProvider>(MockBehavior.Strict);
         private readonly Mock<ICommandCallbackRegistry> _callbackRegistryMock = new Mock<ICommandCallbackRegistry>(MockBehavior.Strict);
@@ -22,9 +41,14 @@ namespace Remiworks.Core.Test.Command.Listener
         [TestInitialize]
         public void Initialize()
         {
-            _callbackRegistryMock
-                .Setup(c => c.AddCallbackForQueue(QueueName, Key, It.IsAny<Action<EventMessage>>()))
-                .Verifiable();
+            _busProviderMock
+                .Setup(b => b.BasicTopicBind(ReplyQueueName, It.Is(MatchingKey)));
+
+            _busProviderMock
+                .Setup(b => b.BasicPublish(It.Is(MatchingReplyEventMessage)));
+
+            _busProviderMock
+                .Setup(b => b.BasicAcknowledge(DeliveryTag, false));
             
             _sut = new CommandListener(_busProviderMock.Object, _callbackRegistryMock.Object);
         }
@@ -32,15 +56,72 @@ namespace Remiworks.Core.Test.Command.Listener
         [TestMethod]
         public async Task SetupCommandListenerCalls_CallbackRegistry()
         {
+            SetupRegistryAddCallbackForQueue();
+            
             await _sut.SetupCommandListenerAsync(QueueName, Key, new Mock<CommandReceivedCallback>().Object, new Mock<Type>().Object);
             
             _callbackRegistryMock.VerifyAll();
         }
 
         [TestMethod]
+        public async Task SetupCommandListenerCalls_BasicTopicBind_WhenMessageComesIn()
+        {
+            Action<EventMessage> registeredAction = null;
+            SetupRegistryAddCallbackForQueue((que, key, action) => registeredAction = action);
+            
+            await _sut.SetupCommandListenerAsync(QueueName, Key, new Mock<CommandReceivedCallback>().Object, typeof(Person));
+            registeredAction(IncomingEventMessage);
+            
+            _busProviderMock.Verify(b => b.BasicTopicBind(ReplyQueueName, It.Is(MatchingKey)));
+        }
+
+        [TestMethod]
+        public async Task SetupCommandListenerCalls_BasicPublish_WhenMessageComesIn()
+        {
+            Action<EventMessage> registeredAction = null;
+            SetupRegistryAddCallbackForQueue((que, key, action) => registeredAction = action);
+            
+            await _sut.SetupCommandListenerAsync(QueueName, Key, new Mock<CommandReceivedCallback>().Object, typeof(Person));
+            registeredAction(IncomingEventMessage);
+            
+            _busProviderMock.Verify(b => b.BasicPublish(It.Is(MatchingReplyEventMessage)));
+        }
+
+        [TestMethod]
+        public async Task SetupCommandListenerCalls_BasicAcknowledge_WhenMessageComesIn()
+        {
+            Action<EventMessage> registeredAction = null;
+            SetupRegistryAddCallbackForQueue((que, key, action) => registeredAction = action);
+            
+            await _sut.SetupCommandListenerAsync(QueueName, Key, new Mock<CommandReceivedCallback>().Object, typeof(Person));
+            registeredAction(IncomingEventMessage);
+            
+            _busProviderMock.Verify(b => b.BasicAcknowledge(DeliveryTag, false));
+        }
+
+        [TestMethod]
         public async Task SetupCommandListenerRegisters_WrappedCallback()
         {
-            await _sut.SetupCommandListenerAsync(QueueName, Key, It.IsAny<CommandReceivedCallback>(), It.IsAny<Type>());
+            Action<EventMessage> registeredAction = null;
+            SetupRegistryAddCallbackForQueue((que, key, action) => registeredAction = action);
+            
+            var waitHandle = new ManualResetEvent(false);
+            object receivedParameter = null;
+            
+            object Callback(object parameter)
+            {
+                waitHandle.Set();
+                receivedParameter = parameter;
+                
+                return null;
+            }
+
+            await _sut.SetupCommandListenerAsync(QueueName, Key, Callback, typeof(Person));
+            registeredAction(IncomingEventMessage);
+
+            waitHandle.WaitOne(2000).ShouldBeTrue();
+            receivedParameter.ShouldBeOfType<Person>();
+            ((Person)receivedParameter).Name.ShouldBe(Person.Name);
         }
 
         [TestMethod]
@@ -54,5 +135,26 @@ namespace Remiworks.Core.Test.Command.Listener
         {
             
         }
+
+        private void SetupRegistryAddCallbackForQueue(Action<string, string, Action<EventMessage>> mockCallback = null)
+        {
+            var setupMock = _callbackRegistryMock.Setup(c => 
+                c.AddCallbackForQueue(QueueName, Key, It.IsAny<Action<EventMessage>>()));
+
+            if (mockCallback != null)
+            {
+                setupMock.Callback(mockCallback);
+            }
+        }
+
+        private static Expression<Func<string[], bool>> MatchingKey =>
+            keys =>
+                keys.Length == 1 &&
+                keys[0] == ReplyKey;
+
+        private static Expression<Func<EventMessage, bool>> MatchingReplyEventMessage =>
+            message =>
+                message.RoutingKey == ReplyKey &&
+                message.CorrelationId == IncomingEventMessage.CorrelationId;
     }
 }
